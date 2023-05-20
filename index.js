46bf743fbcd26e5fa5efacc06531af6c69efe689
Gil C <a348503@uach.mx>
2023-05-20 10:15:34 -0700

    Se agregan muchas pantallas y se enlazan ciertas pantallas con su función

--- index.js
+++ index.js
@@ -39,7 +39,144 @@ function chargePage(file,request,response){
 http.createServer((request,response)=>{
     const file = request.url == '/' ? './www/index.html' : `./WWW${request.url}`;
     //console.log(request.url);
-    if(request.url == "/test"){
+    if(request.url == "/admin_login" && request.method == "POST"){ 
+        console.log("ENTRA");
+        let data = [];
+        request.on('data', value => {
+            data.push(value);
+        }).on('end', ()=>{
+            //console.log(data);
+            let params = Buffer.concat(data).toString();
+            //console.log(params);
+
+            const jsonData = {};
+            params.split('&').forEach(item => {
+            const [key, value] = item.split('=');
+            jsonData[key] = value;
+            });
+
+            console.log(jsonData.admin_usr);
+            /*AQUÍ HAY QUE USAR UNA CONSULTA PARA VERIFICAR QUE EL USUARIO SEA ADMIN MIENTRAS LO 
+            VOY A HARDCODEAR PARA SIMULAR LA FUNCIÓN*/
+            let user = "admin";
+            let password = "ispira";
+            /*Aquí procedemos a en caso de que sí sea cargar el main_screen.html si no cargamos la misma index */
+            
+            if(user == jsonData.admin_usr && password == jsonData.pswrd){
+                response.writeHead(302, { 'Location': './main_screen.html' });
+                response.end();
+            }
+            else{
+                response.writeHead(302, { 'Location': './index.html' });
+                response.end();
+            }
+
+            //console.log(params);
+            //response.write(params);
+        });
+    }
+    else if(request.url == "/user_visit" && request.method == "POST"){
+        //consultar el usuario a la base de datos y desplegar la página de bienvenido
+        let data = [];
+        request.on('data', value => {
+            data.push(value);
+        }).on('end', ()=>{
+            //console.log(data);
+            let params = Buffer.concat(data).toString();
+            //console.log(params);
+
+            const jsonData = {};
+            params.split('&').forEach(item => {
+            const [key, value] = item.split('=');
+            jsonData[key] = value;
+            });
+
+            console.log(jsonData.admin_usr);
+            /*AQUÍ HAY QUE USAR UNA CONSULTA PARA VERIFICAR QUE EL USUARIO EXISTE MIENTRAS HARDCODE*/
+            let user = jsonData.usr;
+            let userName = "Gil";
+            console.log(jsonData);
+            let userExists = true;
+            /*Aquí procedemos a en caso de que sí existe, a cargar la página de bienvenido */
+            
+            if(userExists){
+                let ejsFile = './www/ejsFiles/welcome.ejs';
+                //console.log(ejsFile)
+                ejs.renderFile(ejsFile, { "username" : userName , "days": 1234 }, (err, renderedHtml) => {
+                    if (err) {
+                    response.statusCode = 500;
+                    response.end('Error interno del servidor');
+                    return;
+                    }
+                    response.statusCode = 200;
+                    response.setHeader('Content-Type', 'text/html');
+                    response.end(renderedHtml);
+                    //estaría bueno en welcome.ejs poner un Script que después de un tiempo de q 5 segundos haga
+                    //un request para volver a cargar main_screen.html
+                });
+            }
+            else{
+                response.writeHead(302, { 'Location': './main_screen.html' });
+                response.end();
+            }
+
+            //console.log(params);
+            //response.write(params);
+        });
+    }
+    else if(request.url == "/get_user_info" && request.method == "POST"){
+        let data = [];
+        request.on('data', value => {
+            data.push(value);
+        }).on('end', ()=>{
+            //console.log(data);
+            let params = Buffer.concat(data).toString();
+            //console.log(params);
+
+            const jsonData = {};
+            params.split('&').forEach(item => {
+            const [key, value] = item.split('=');
+            jsonData[key] = value;
+            });
+            /*AQUÍ HAY QUE USAR UNA CONSULTA PARA VERIFICAR QUE EL USUARIO EXISTE MIENTRAS HARDCODE*/
+            let user = jsonData.usr;
+
+            //tendríamos los resultados de la consulta y los pasaríamos a un Json
+            let userInfo = { "user_id":user, "username": "Gil", "birthday": "07/11/2002", "direction":"UACH CAMPUS II",
+            "aka":"Campanita666", "photo_source":"./recursos/algo así", "start_date":"15/09/2018","total_visits":"666"
+            , "last_visit":"20/05/2023","status":"active" };
+            let userName = "Gil";
+            console.log(jsonData);
+            let userExists = true;
+            /*Aquí procedemos a en caso de que sí existe, a cargar los datos */
+
+            
+            if(userExists){
+                let ejsFile = './www/ejsFiles/usr_info.ejs';
+                //console.log(ejsFile)
+                ejs.renderFile(ejsFile, userInfo, (err, renderedHtml) => {
+                    if (err) {
+                    response.statusCode = 500;
+                    response.end('Error interno del servidor');
+                    return;
+                    }
+                    response.statusCode = 200;
+                    response.setHeader('Content-Type', 'text/html');
+                    response.end(renderedHtml);
+                    //estaría bueno en welcome.ejs poner un Script que después de un tiempo de q 5 segundos haga
+                    //un request para volver a cargar main_screen.html
+                });
+            }
+            else{
+                response.writeHead(302, { 'Location': './user_consult.html' });
+                response.end();
+            }
+
+            //console.log(params);
+            //response.write(params);
+        });
+    }
+    else if(request.url == "/test"){
         let ejsFile = './www/ejsFiles/test.ejs';
         console.log(ejsFile)
         let nombre = "Campanita"
